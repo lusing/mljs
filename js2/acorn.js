d15ca75ab512be9df7750540f9e2073a3f8ea8da
旭伦 <ziying.liuziying@alibaba-inc.com>
2021-10-11 19:48:16 +0800

    update

--- js2/acorn.js
+++ js2/acorn.js
@@ -1,4 +1,3 @@
 let acorn = require("acorn");
-//for(let i=0;i<10;i+=1){console.log(i);}
 
 console.log(acorn.parse("for(let i=0;i<10;i+=1){console.log(i);}", {ecmaVersion: 2020}));
---
+++ python/cartpole1.py
@@ -0,0 +1,18 @@
+import gym
+
+def cartpole():
+    environment = gym.make('CartPole-v1')
+    environment.reset()
+    for i in range(1000):
+        environment.render()
+        action = environment.action_space.sample()
+        observation, reward, done, info = environment.step(action)
+        print("Step {}:".format(i))
+        print("action: {}:".format(action))
+        print('observation: {}'.format(observation))
+        print('reward: {}'.format(reward))
+        print('done: {}'.format(done))
+        print('info: {}'.format(info))
+
+if __name__ == '__main__':
+    cartpole()
