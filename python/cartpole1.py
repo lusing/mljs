e47c5dacaeb35d6ea950394184008a7ec20edd5c
旭伦 <ziying.liuziying@alibaba-inc.com>
2021-10-11 19:52:35 +0800

    update

--- python/cartpole1.py
+++ python/cartpole1.py
@@ -4,7 +4,7 @@ def cartpole():
     environment = gym.make('CartPole-v1')
     environment.reset()
     for i in range(1000):
-        environment.render()
+        # environment.render()
         action = environment.action_space.sample()
         observation, reward, done, info = environment.step(action)
         print("Step {}:".format(i))
@@ -13,6 +13,8 @@ def cartpole():
         print('reward: {}'.format(reward))
         print('done: {}'.format(done))
         print('info: {}'.format(info))
+        if done:
+            break
 
 if __name__ == '__main__':
     cartpole()
