a17d909052555b92d2646fa7ae3e6cf751456fed
Xulun <lusinga@126.com>
2021-11-16 01:30:36 +0800

    update dqn

--- python/tf1.py
+++ python/tf1.py
@@ -1,4 +1,5 @@
 import random
+import re
 import gym
 import math
 import numpy as np
@@ -54,3 +55,39 @@ class DQN:
             return self.env.action_space.sample()
         else:
             return np.argmax(self.model.predict(state))
+
+    def preprocess_state(self, state):
+        return np.reshape(state, [1, self.input_size])
+
+    def train(self):
+        scores = deque(maxlen=10000)
+        avg_scores = []
+        for e in range(EPOCHS):
+            state = self.env.reset()
+            state = self.preprocess_state(state)
+            done = False
+            i = 0
+            while not done:
+                action = self.choose_action(state, self.epsilon)
+                next_state, reward, done, _ = self.env.step(action)
+                next_state = self.preprocess_state(next_state)
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+                self.epsilon = max(self.epsilon_min, self.epsilon_decay * self.epsilon)
+                i += 1
+                scores.append(i)
+                mean_score = np.mean(scores)
+                avg_scores.append(mean_score)
+                if mean_score >= THRESHOLD and e >= 100:
+                    print('Ran {} episodes. Solved after {} trials'.format(e, e-100))
+                    return avg_scores
+                if e % 100 == 0:
+                    print('Episode {}'.format(e))
+                
+                self.replay(self.batch_size)
+                return avg_scores
+
+env_string = 'CartPole-v0'
+agent = DQN(env_string)
+scores1 = agent.train()
+print(scores1)
